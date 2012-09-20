536812f86e7fed28136f8a6daa17244d9bb7a8dc
Juan Ernesto Crawford Naranjo <juancrawford92@gmail.com>
2012-09-20 00:58:49 -0600

    GUI

---
+++ Interfaz_WPF/Login_WPF/Login.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace Login_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+    public partial class Login : Window
+    {
+        public int NoCierre = 0;
+
+        public Login()
+        {
+            InitializeComponent();
+        }        
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Welcome welcome = new Welcome();
+            welcome.Show();
+            NoCierre = 1;
+            Close();
+        }
+
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            NoCierre = 1;
+            Close();
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (NoCierre == 0)
+            {
+                MessageBoxResult result = MessageBox.Show("Desea cerrar la aplicación?", "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ Interfaz_WPF/Login_WPF/Welcome.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+using System.Windows.Shapes;
+
+namespace Login_WPF
+{
+    /// <summary>
+    /// Interaction logic for Welcome.xaml
+    /// </summary>
+    public partial class Welcome : Window
+    {
+        public int NoCierre = 0;
+
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (NoCierre == 0)
+            {
+                MessageBoxResult result = MessageBox.Show("Desea cerrar la aplicación?", "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    MessageBoxResult res = MessageBox.Show(this, "Si cierra la ventana, todos los Datos no guardados pueden ser perdidos.", "Confirmación", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+                    if (res == MessageBoxResult.OK)
+                    {
+                        Application.Current.Shutdown();
+                    }
+                    else
+                    {
+                        e.Cancel = true;
+                    }
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Esta seguro que desea cerrar sesión?";
+            string caption = "Confirmación";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Question;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes)
+            {
+                Login login = new Login();
+                login.Show();
+                NoCierre = 1;
+                Close();
+            }
+        }
+
+        private void buttonAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Desea agregar esta nueva cuenta?";
+            string caption = "Confirmación";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Question;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+
+        private void buttonModificar_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Esta seguro que desea modificar la cuenta seleccionada?";
+            string caption = "Confirmación";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+
+        private void buttonEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Esta seguro que desea eliminar la cuenta seleccionada?";
+            string caption = "Confirmación";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+
+        private void buttonGuardarAsiento_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Desea guardar el asiento creado?";
+            string caption = "Confirmación";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Question;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+
+        private void buttonAnularAsiento_Click(object sender, RoutedEventArgs e)
+        {
+            string message = "Esta seguro que desea anular el asiento seleccionado?";
+            string caption = "Confirmación";
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            if (MessageBox.Show(message, caption, buttons, icon) == MessageBoxResult.Yes)
+            {
+
+            }
+        }
+    }
+}
