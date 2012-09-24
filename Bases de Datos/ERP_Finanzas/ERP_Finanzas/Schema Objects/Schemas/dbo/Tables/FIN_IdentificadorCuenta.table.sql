512ca7ecf4f99eefc9fd2991c1c1b384aaf7db08
Raúl Madrigal Acuña <raul3_15@outlook.com>
2012-09-23 22:38:02 -0600

    ERP_Finanzas Basic Data
    Datos iniciales ERP_Finanzas

--- Bases de Datos/ERP_Finanzas/ERP_Finanzas/Schema Objects/Schemas/dbo/Tables/FIN_IdentificadorCuenta.table.sql
+++ Bases de Datos/ERP_Finanzas/ERP_Finanzas/Schema Objects/Schemas/dbo/Tables/FIN_IdentificadorCuenta.table.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[FIN_IdentificadorCuenta] (
     [IdidentificadorCuenta] INT          IDENTITY (1, 1) NOT NULL,
-    [Nombre]                VARCHAR (30) NOT NULL
+    [Nombre]                VARCHAR (50) NOT NULL,
+	[Numero]				INT			 NOT NULL
 );
 
--- Bases de Datos/ERP_Finanzas/ERP_Finanzas/Scripts/Post-Deployment/BasicData.sql
+++ Bases de Datos/ERP_Finanzas/ERP_Finanzas/Scripts/Post-Deployment/BasicData.sql
@@ -12,6 +12,7 @@
 -----------------------------------------------------------
 INSERT INTO dbo.FIN_TipoAsiento VALUES ('AS')
 
+GO
 -- =============================================
 -- FIN_Identificador
 -- =============================================
@@ -20,3 +21,11 @@ INSERT INTO dbo.FIN_TipoAsiento VALUES ('AS')
 -- Fecha: 23/09/2012
 -- Descripcion: Primeros valores para Identificadores de cuenta
 -----------------------------------------------------------
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Activo', 1)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Pasivo', 2)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Patrimonio', 3)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Ingresos', 4)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Costos', 5)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Gastos', 6)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Otros Ingresos', 7)
+INSERT INTO dbo.FIN_IdentificadorCuenta(Nombre, Numero) VALUES ('Otros Gastos', 8)
