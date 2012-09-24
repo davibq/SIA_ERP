f0223880f062e87195fdf447d8015ca7e8f4c5bb
Raúl Madrigal Acuña <raul3_15@outlook.com>
2012-09-23 19:49:05 -0600

    Sps ERP_Principal
    Obtiene información  y valida entidades, modulos y usuarios.

---
+++ Bases de Datos/ERP_Principal/ERP_Principal/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ERPSP_ObtenerDatosEntidad.proc.sql
@@ -0,0 +1,17 @@
+﻿-----------------------------------------------------------
+-- Autor: Rmadrigal
+-- Fecha: 23/09/2012
+-- Descripcion: Obtiene los datos de una entidad 
+-----------------------------------------------------------
+CREATE PROCEDURE [dbo].[ERPSP_ObtenerDatosEntidad]
+	@Entidad VARCHAR(35)
+AS 
+	SELECT Ent.Nombre Entidad, Ent.CedulaJuridica, Ent.Logo, Pai.Nombre Pais, Con.Nombre Contacto, Con.Descripcion ValorContacto
+	FROM dbo.ERP_Entidades Ent
+	INNER JOIN dbo.ERP_ContactoXEntidad ConXEnt ON ConXEnt.IdEntidad = Ent.IdEntidad
+	INNER JOIN dbo.ERP_Contacto Con ON Con.IdContacto = ConXEnt.IdContacto
+	INNER JOIN dbo.ERP_EntidadXPais EntXPai ON EntXPai.IdEntidad = Ent.IdEntidad
+	INNER JOIN dbo.ERP_Paises Pai ON Pai.IdPais = EntXPai.IdPais
+	WHERE Ent.Nombre = @Entidad AND Ent.Enabled = 1 AND EntXPai.Enabled = 1
+	AND ConXEnt.Enabled = 1 AND Con.Enabled = 1
+RETURN 0
