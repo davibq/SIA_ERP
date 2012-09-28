8cfc121d7caae25b767846d0b7b5cb6b1902277e
Raul Madrigal Acuña <raulmacu3.15@gmail.com>
2012-09-27 20:02:08 -0600

    SPs ERP_Finanzas
    Actualiza cuentas, verifica estado mes, obtiene asientos de un periodo

---
+++ Bases de Datos/ERP_Finanzas/ERP_Finanzas/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ERPSP_ObtenerAsientosPeriodoContable.proc.sql
@@ -0,0 +1,25 @@
+﻿-----------------------------------------------------------
+-- Autor: Rmadrigal
+-- Fecha: 27/09/2012
+-- Descripcion: Obtiene la informacíon de asientos activos de un periodo contable
+-----------------------------------------------------------
+CREATE PROCEDURE [dbo].[ERPSP_ObtenerAsientosPeriodoContable]
+	@Anyo	INT
+AS 
+	SELECT Asi.Debe, Asi.Haber, Asi.FechaContabilizacion, Asi.FechaDocumento, TipoAsi.Nombre NombreTipo, TipoAsi.Descripcion DescripcionTipo,
+	Asi.Referencia1, Asi.Referencia2, Cuen.Codigo, Cuen.Nombre, CuXAs.AlDebe, MonXMonXAsi.Monto, Mon.Acronimo,
+	CASE
+		WHEN Mon.EsLocal = 1 THEN 'Local'
+		WHEN Mon.EsSistema = 1 THEN 'Sistema'
+		ELSE ''
+	END MonedaAplica ,Hist.TipoCambio, Hist.Fecha FechaTipoCambio
+	FROM dbo.FIN_Asiento Asi
+	INNER JOIN dbo.FIN_PeriodoContable PerCon ON PerCon.IdPeriodoContable = Asi.IdPeriodoContable
+	INNER JOIN dbo.FIN_TipoAsiento TipoAsi ON TipoAsi.IdTipoAsiento = Asi.IdTipoAsiento
+	INNER JOIN dbo.FIN_CuentasXAsiento CuXAs ON CuXAs.IdAsiento = Asi.IdAsiento
+	INNER JOIN dbo.FIN_Cuenta Cuen ON Cuen.IdCuenta = CuXAs.IdCuenta
+	INNER JOIN dbo.FIN_MontosXMonedaXAsientos MonXMonXAsi ON MonXMonXAsi.IdAsiento = Asi.IdAsiento AND Cuen.IdCuenta = MonXMonXAsi.IdCuenta
+	INNER JOIN dbo.FIN_Moneda Mon ON Mon.IdMoneda = MonXMonXAsi.IdMoneda
+	INNER JOIN dbo.FIN_HistorialTipoCambio Hist ON Hist.IdMonedaOrigen = Mon.IdMoneda AND Hist.IdMonedaDestino = Mon.IdMoneda
+	WHERE Asi.Enabled = 1 AND Cuen.Enabled = 1 AND PerCon.Anyo = Anyo
+RETURN 0
---
+++ Bases de Datos/ERP_Finanzas/ERP_Finanzas/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ERPSP_VerificarEstadoMes.proc.sql
@@ -0,0 +1,12 @@
+﻿-----------------------------------------------------------
+-- Autor: Rmadrigal
+-- Fecha: 27/09/2012
+-- Descripcion: Verifica el estado de un mes
+-----------------------------------------------------------
+CREATE PROCEDURE [dbo].[ERPSP_VerificarEstadoMes]
+	@Fecha DATE
+AS
+	SELECT EstMes.Descripcion FROM dbo.FIN_Mes Mes
+	INNER JOIN dbo.FIN_EstadoMes EstMes ON EstMes.IdEstadoMes = Mes.IdEstadoMes
+	WHERE MONTH(Mes.FechaInicio) = MONTH(@Fecha) AND YEAR(Mes.FechaInicio) = YEAR(@Fecha) 
+RETURN 0
