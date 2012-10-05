f30116d99f22516ba7fde64da98dffb3e8e921d8
David Ballesteros <davibq@gmail.com>
2012-10-04 21:40:55 -0600

    Libreria

---
+++ DataContracts/LibreriaContabilidad/Cuenta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace SIA.Contabilidad.Libreria
+{
+    [DataContract]
+    public class Cuenta
+    {
+        [DataMember]
+        public int CuentaId { get; set; }
+
+        [DataMember]
+        public Cuenta CuentaPadre { get; set; }
+
+        [DataMember]
+        public string Nombre { get; set; }
+
+        [DataMember]
+        public string Codigo { get; set; }
+
+        [DataMember]
+        public int Nivel { get; set; }
+
+        [DataMember]
+        public double MontoDebe { get; set; }
+
+        [DataMember]
+        public double MontoHaber { get; set; }
+    }
+}
--- SIA_ERP/DataAccessFinanzas/DAFinanzas.cs
+++ SIA_ERP/DataAccessFinanzas/DAFinanzas.cs
@@ -9,10 +9,26 @@ namespace DataAccessFinanzas
 {
     public class DAFinanzas : DataAccess
     {
+
+        #region Constructor
+
+        /// <summary>
+        /// Instancia el connection string con el nombre de la BD que el usuario escogio.
+        /// </summary>
+        /// <param name="pBaseDatos">Nombre de la base de datos</param>
         public DAFinanzas(string pBaseDatos)
             : base(string.Format(ConfigurationManager.ConnectionStrings["Clientes"].ConnectionString, pBaseDatos))
         {
-            
+
         }
+
+        #endregion
+
+        #region Metodos
+
+
+
+        #endregion
+
     }
 }
