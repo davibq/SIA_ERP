db4ad4185e4f952b7378ab605c026f08188c4d3e
David Ballesteros <davibq@gmail.com>
2012-10-18 20:26:05 -0600

    SP MonedasXCuenta

---
+++ SIA_ERP/Bases de Datos/ERP_Finanzas/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/ObtenerMonedasCuenta.proc.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[ObtenerMonedasCuenta]
+	@pNombreCuenta VARCHAR(50)
+AS
+	SELECT Mo.Nombre, Mo.CodigoBCCR, Mo.Acronimo FROM FIN_Moneda Mo
+	INNER JOIN FIN_SaldoXCuentaXMoneda SXC ON SXC.IdMoneda=Mo.IdMoneda
+	INNER JOIN FIN_Cuenta Cu ON SXC.IdMoneda=sxc.IdMoneda
+	WHERE Cu.Nombre=@pNombreCuenta AND Cu.IdCuenta=SXC.IdCuenta
+RETURN 0
