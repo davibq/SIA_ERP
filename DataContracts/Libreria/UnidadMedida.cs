7af773a5c996c15fe5353fe58baf41171a38ac14
Josue Santamaria Agüero <josuesanta08@gmail.com>
2012-11-25 10:53:29 -0600

    libreria

---
+++ DataContracts/Libreria/UnidadMedida.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace SIA.Libreria
+{
+
+    [DataContract]
+    class UnidadMedida
+    {
+        [DataMember]
+        public string Nombre { get; set; }
+    }
+}
