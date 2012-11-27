e266ecef63b87c1e64425d473c764178f1fb12a0
Josue Santamaria Agüero <josuesanta08@gmail.com>
2012-11-27 03:14:04 -0600

    sp y libreria

---
+++ SIA_ERP/LibreriaVentaCompra/Transferencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using SIA.Libreria;
+
+namespace SIA.VentaCompra.Libreria
+{
+    [DataContract]
+    public class Transferencia
+    {
+        [DataMember]
+        public int idTransferencia { get; set; }
+        [DataMember]
+        public string TipoTransferencia { get; set; }
+        [DataMember]
+        public SocNegocio Socio { get; set; }
+        [DataMember]
+        public string NumTranseferencia { get; set; }
+        [DataMember]
+        public double Monto { get; set; }
+        [DataMember]
+        public Banco banco { get; set; }
+        [DataMember]
+        public DateTime Fecha { get; set; }
+
+    }
+}
