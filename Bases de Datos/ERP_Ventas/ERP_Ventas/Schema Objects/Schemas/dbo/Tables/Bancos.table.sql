674ab780c3ed7d7b8d2a0823c347c88227c2bea0
Josue Santamaria Agüero <josuesanta08@gmail.com>
2012-11-26 23:47:48 -0600

    nuevos datos

---
+++ Bases de Datos/ERP_Ventas/ERP_Ventas/Schema Objects/Schemas/dbo/Tables/Bancos.table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Bancos]
+(
+	[idBanco]		int			IDENTITY(1,1) NOT NULL,
+	[Nombre]		varchar(50) NOT NULL,
+	[Moneda]		varchar(3)	NOT NULL,
+	[NoCuenta]		varchar(25) NOT NULL,
+	[CuentaMayor]	varchar(25) NOT NULL
+);
---
+++ Bases de Datos/ERP_Ventas/ERP_Ventas/Schema Objects/Schemas/dbo/Tables/Keys/FK_Transferencia_Bancos.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Transferencia]  ADD  CONSTRAINT [FK_Transferencia_Bancos] FOREIGN KEY([idBanco])
+REFERENCES [dbo].[Bancos] ([idBanco]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
